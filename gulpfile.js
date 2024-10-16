e6c10b75ee045de9e29baf7d6732c82f514e75e4
Nathan G Silva <nathangsilva@MacBook-Pro-de-Nathan.local>
2024-10-16 14:58:38 -0300

    adjusts

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,21 @@ const sass = require('gulp-sass')(require('sass'));
 const { series, watch } = gulp;
 
 const paths = {
-  scss: 'src/scss/app.scss', 
-  css: 'src/css/'     
+  scss: 'dist/scss/app.scss', // Arquivo SCSS principal
+  css: 'dist/css/'             // Destino do CSS compilado
 };
 
+// Função para compilar SCSS para CSS
 function compileSass() {
-  return gulp.src(paths.scss)
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest(paths.css));
+  return gulp.src(paths.scss) // Usando gulp.src para pegar o arquivo SCSS
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Compilando o SCSS
+    .pipe(gulp.dest(paths.css)); // Salvando o CSS compilado
 }
 
+// Função para monitorar alterações
 function watchFiles() {
-  watch('src/scss/**/*.scss', compileSass);
+  watch('dist/scss/**/*.scss', compileSass); // Observa mudanças em todos os arquivos SCSS
 }
 
+// Tarefa padrão
 exports.default = series(compileSass, watchFiles);
