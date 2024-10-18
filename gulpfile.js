213c50865a57c69f99b1829a76baaf7fa4d6393e
Nathan G Silva <nathangsilva@MacBook-Pro-de-Nathan.local>
2024-10-17 22:11:27 -0300

    Adjust

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const { series, watch } = gulp;
 
 const paths = {
-  scss: 'dist/scss/**/*.scss',  // Monitorar todos os arquivos SCSS
-  css: 'dist/css/'              // Salvar o CSS compilado aqui
+  scss: 'public/dist/scss/**/*.scss',  // Monitorar todos os arquivos SCSS em public/dist
+  css: 'public/dist/css/'              // Salvar o CSS compilado em public/dist/css
 };
 
 function compileSass() {
   return gulp.src(paths.scss, { allowEmpty: true })  // Permitir arquivos vazios
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) // Compilar SCSS
-    .pipe(gulp.dest(paths.css));  // Salvar CSS
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))  // Compilar SCSS
+    .pipe(gulp.dest(paths.css));  // Salvar CSS em public/dist/css
 }
 
 function watchFiles() {
